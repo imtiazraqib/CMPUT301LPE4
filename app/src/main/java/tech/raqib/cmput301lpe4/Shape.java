55c1827eb6ace2d6310516a6e444b104e0367428
cjmiller <cjmiller@ualberta.ca>
2019-01-30 18:59:11 -0700

    Update Shape.java

--- app/src/main/java/tech/raqib/cmput301lpe4/Shape.java
+++ app/src/main/java/tech/raqib/cmput301lpe4/Shape.java
@@ -1,9 +1,9 @@
 package tech.raqib.cmput301lpe4;
 
 public abstract class Shape {
-    private int x;
-    private int y;
-    private int z;
+    private int X;
+    private int Y;
+    private int Z;
     private String color;
 
     public Shape() {
