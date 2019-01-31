aef8022cfc3374290ed9dd61c7494147231244cd
imtiazraqib <imtiazud@ualberta.ca>
2019-01-30 18:06:23 -0700

    Adding the Abstract Shape Class

---
+++ app/src/main/java/tech/raqib/cmput301lpe4/MainActivity.java
@@ -0,0 +1,13 @@
+package tech.raqib.cmput301lpe4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/tech/raqib/cmput301lpe4/Shape.java
@@ -0,0 +1,10 @@
+package tech.raqib.cmput301lpe4;
+
+public abstract class Shape {
+    private int x;
+    private int y;
+
+    public Shape() {
+
+    }
+}
