aa6a9602a5feb4af2f57924ad021a8032b408a51
keveren <kevin6@ualberta.ca>
2019-01-30 19:12:13 -0700

    Merge branch 'master' of https://github.com/imtiazraqib/CMPUT301LPE4

--- app/src/main/java/tech/raqib/cmput301lpe4/Shape.java
+++ app/src/main/java/tech/raqib/cmput301lpe4/Shape.java
@@ -1,10 +1,10 @@
 package tech.raqib.cmput301lpe4;
 
 public abstract class Shape {
-    private int x;
-    private int y;
-    private int z;
-    private String color = "Cyan";
+    private int X;
+    private int Y;
+    private int Z;
+    private String teal;
 
     public Shape() {
 
